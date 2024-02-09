587279f30d171de6303b1b46aa6e4a4717a4b18a
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2024-02-09 09:48:36 +0100

    (fix): error now silently when banner not found

--- src/helpers/a11y-helpers.ts
+++ src/helpers/a11y-helpers.ts
@@ -6,21 +6,21 @@
  * @return {Element|null} Transformed element
  */
 export const transformTag = (element: Element, transformTo: string) => {
-  if (!element) return null;
+	if (!element) return null;
 
-  const transformedElement: Element = document.createElement(transformTo);
+	const transformedElement: Element = document.createElement(transformTo);
 
-  transformedElement.innerHTML = element.innerHTML;
+	transformedElement.innerHTML = element.innerHTML;
 
-  for (let i = 0; i < element.attributes.length; i++) {
-    transformedElement.setAttribute(element.attributes[i].name, element.attributes[i].value);
-  }
+	for (let i = 0; i < element.attributes.length; i++) {
+		transformedElement.setAttribute(element.attributes[i].name, element.attributes[i].value);
+	}
 
-  if (!element.parentNode) return transformedElement;
+	if (!element.parentNode) return transformedElement;
 
-  element.parentNode.replaceChild(transformedElement, element);
+	element.parentNode.replaceChild(transformedElement, element);
 
-  return transformedElement;
+	return transformedElement;
 };
 
 /**
@@ -28,29 +28,30 @@ export const transformTag = (element: Element, transformTo: string) => {
  *
  * @param {string} selector The CSS selector for the element to wait for
  * @param {number} maxTimeToSearch  The maximum time to wait for the element, in milliseconds
- * @return {Promise<NodeListOf<Element> | Element>} A promise that resolves with the element when it appears in the DOM
+ * @return {Promise<NodeListOf<Element> | Element | null>} A promise that resolves with the element when it appears in the DOM
  * @throws {string} If the element does not appear in the DOM before the timeout is reached
  */
 export const waitForElement = (
-  selector: string,
-  maxTimeToSearch: number = 5000
-): Promise<NodeListOf<Element> | Element> => {
-  return new Promise((resolve, reject) => {
-    const startTime = Date.now();
-    const interval = setInterval(() => {
-      const elements = document.querySelectorAll(selector),
-        hasElements = elements.length > 0,
-        isSingleElement = elements.length === 1;
+	selector: string,
+	maxTimeToSearch: number = 5000
+): Promise<NodeListOf<Element> | Element | null> => {
+	return new Promise((resolve) => {
+		const startTime = Date.now();
+		const interval = setInterval(() => {
+			const elements = document.querySelectorAll(selector),
+				hasElements = elements.length > 0,
+				isSingleElement = elements.length === 1;
 
-      if (hasElements) {
-        clearInterval(interval);
-        resolve(isSingleElement ? elements[0] : elements);
-      } else if (Date.now() - startTime >= maxTimeToSearch) {
-        clearInterval(interval);
-        reject(new Error(`Timeout exceeded. Element '${selector}' not found within timelimit.`));
-      }
-    }, 100);
-  });
+			if (hasElements) {
+				clearInterval(interval);
+				resolve(isSingleElement ? elements[0] : elements);
+			} else if (Date.now() - startTime >= maxTimeToSearch) {
+				clearInterval(interval);
+				// Should be reject(); but that wont silently fail which is what we want.
+				resolve(null);
+			}
+		}, 100);
+	});
 };
 
 /**
@@ -61,21 +62,21 @@ export const waitForElement = (
  * @param {Element[]} elements The elements to check for visibility
  */
 export const checkCanFocusTrap = (elements: Element[]) => {
-  const results = elements.map((element): Promise<void> => {
-    return new Promise((resolve) => {
-      const interval = setInterval(() => {
-        const style = window.getComputedStyle(element);
-        if (
-          style.visibility !== 'hidden' &&
-          style.display !== 'none' &&
-          style.height !== '0px' &&
-          style.width !== '0px'
-        ) {
-          resolve();
-          clearInterval(interval);
-        }
-      }, 5);
-    });
-  });
-  return Promise.all(results);
+	const results = elements.map((element): Promise<void> => {
+		return new Promise((resolve) => {
+			const interval = setInterval(() => {
+				const style = window.getComputedStyle(element);
+				if (
+					style.visibility !== 'hidden' &&
+					style.display !== 'none' &&
+					style.height !== '0px' &&
+					style.width !== '0px'
+				) {
+					resolve();
+					clearInterval(interval);
+				}
+			}, 5);
+		});
+	});
+	return Promise.all(results);
 };
